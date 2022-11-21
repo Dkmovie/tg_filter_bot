e475e502f5f567c4f86f67632f112f7a8179ce4b
Dkmovie <99384833+Dkmovie@users.noreply.github.com>
2022-11-21 18:29:32 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,7 @@
-
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/Dkmovie/tg_filter_bot /Eva
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
-fi
-cd /Eva
+echo "Cloning Repo, Please Wait..."
+git clone -b master https://github.com/Dkmovie/tgmoviebot.git /IMDb-Movie-Bot
+cd /IMDb-Movie-Bot
+echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting Bot, Please Wait..."
 python3 bot.py
